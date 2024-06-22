d68ffd2d61bb58b2715351d0569fc7076cb7d8a8
Sadra Rahbar <48208938+sadrarahbar@users.noreply.github.com>
2024-06-23 00:50:10 +0330

    feat: change 'fonts and styles locations'

--- src/app.tsx
+++ src/app.tsx
@@ -6,8 +6,8 @@ import { SettingDrawer } from '@ant-design/pro-components';
 import type { RunTimeLayoutConfig } from '@umijs/max';
 import { history, Link } from '@umijs/max';
 import defaultSettings from '../config/defaultSettings';
+import './assets/styles/IranSansFont.css';
 import { errorConfig } from './requestErrorConfig';
-import './styles/IranSansFont.css';
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
