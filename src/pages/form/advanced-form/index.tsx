fea8b88e1bc8ec5312b56600f30d764babedba9c
maryammsaeidi <104352443+maryammsaeidi@users.noreply.github.com>
2024-06-26 07:24:49 +0330

    feat: changes 'form'

---
+++ src/pages/form/advanced-form/index.tsx
@@ -0,0 +1,538 @@
+import { CloseCircleOutlined } from '@ant-design/icons';
+import type { ProColumnType } from '@ant-design/pro-components';
+import {
+  EditableProTable,
+  FooterToolbar,
+  PageContainer,
+  ProForm,
+  ProFormDateRangePicker,
+  ProFormSelect,
+  ProFormText,
+  ProFormTimePicker,
+} from '@ant-design/pro-components';
+import { Card, Col, Popover, Row, message } from 'antd';
+import type { FC } from 'react';
+import { useState } from 'react';
+import { fakeSubmitForm } from './service';
+import useStyles from './style.style';
+interface TableFormDateType {
+  key: string;
+  workId?: string;
+  name?: string;
+  department?: string;
+  isNew?: boolean;
+  editable?: boolean;
+}
+type InternalNamePath = (string | number)[];
+const fieldLabels = {
+  name: 'نام انبار',
+  url: 'نام دامنه انبار',
+  owner: 'مدیر انبار',
+  approver: 'تصویب کننده',
+  dateRange: 'از تاریخ تا تاریخ',
+  type: 'نوع انبار',
+  name2: 'نام وظیفه',
+  url2: 'جزئیات ماموریت',
+  owner2: 'مجری',
+  approver2: 'مسئول',
+  dateRange2: 'تاریخ اجرا',
+  type2: 'نوع وظیفه',
+};
+const tableData = [
+  {
+    key: '1',
+    workId: '00001',
+    name: 'John Brown',
+    department: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    workId: '00002',
+    name: 'Jim Green',
+    department: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    workId: '00003',
+    name: 'Joe Black',
+    department: 'Sidney No. 1 Lake Park',
+  },
+];
+interface ErrorField {
+  name: InternalNamePath;
+  errors: string[];
+}
+const AdvancedForm: FC<Record<string, any>> = () => {
+  const { styles } = useStyles();
+  const [error, setError] = useState<ErrorField[]>([]);
+  const getErrorInfo = (errors: ErrorField[]) => {
+    const errorCount = errors.filter((item) => item.errors.length > 0).length;
+    if (!errors || errorCount === 0) {
+      return null;
+    }
+    const scrollToField = (fieldKey: string) => {
+      const labelNode = document.querySelector(`label[for="${fieldKey}"]`);
+      if (labelNode) {
+        labelNode.scrollIntoView(true);
+      }
+    };
+    const errorList = errors.map((err) => {
+      if (!err || err.errors.length === 0) {
+        return null;
+      }
+      const key = err.name[0] as 'name' | 'url' | 'owner' | 'approver' | 'dateRange' | 'type';
+      return (
+        <li key={key} className={styles.errorListItem} onClick={() => scrollToField(key)}>
+          <CloseCircleOutlined className={styles.errorIcon} />
+          <div className={styles.errorMessage}>{err.errors[0]}</div>
+          <div className={styles.errorField}>{fieldLabels[key]}</div>
+        </li>
+      );
+    });
+    return (
+      <span className={styles.errorIcon}>
+        <Popover
+          title="اطلاعات تایید فرم"
+          content={errorList}
+          overlayClassName={styles.errorPopover}
+          trigger="click"
+          getPopupContainer={(trigger: HTMLElement) => {
+            if (trigger && trigger.parentNode) {
+              return trigger.parentNode as HTMLElement;
+            }
+            return trigger;
+          }}
+        >
+          <CloseCircleOutlined />
+        </Popover>
+        {errorCount}
+      </span>
+    );
+  };
+  const onFinish = async (values: Record<string, any>) => {
+    setError([]);
+    try {
+      await fakeSubmitForm(values);
+      message.success('با موفقیت ثبت شد');
+    } catch {
+      // console.log
+    }
+  };
+  const onFinishFailed = (errorInfo: any) => {
+    setError(errorInfo.errorFields);
+  };
+  const columns: ProColumnType<TableFormDateType>[] = [
+    {
+      title: 'نام عضو',
+      dataIndex: 'name',
+      key: 'name',
+      width: '20%',
+    },
+    {
+      title: 'شماره شغل',
+      dataIndex: 'workId',
+      key: 'workId',
+      width: '20%',
+    },
+    {
+      title: 'بخش',
+      dataIndex: 'department',
+      key: 'department',
+      width: '40%',
+    },
+    {
+      title: 'عملیات',
+      key: 'action',
+      valueType: 'option',
+      render: (_, record: TableFormDateType, index, action) => {
+        return [
+          <a
+            key="eidit"
+            onClick={() => {
+              action?.startEditable(record.key);
+            }}
+          >
+            ویرایش کنید
+          </a>,
+        ];
+      },
+    },
+  ];
+  return (
+    <ProForm
+      layout="vertical"
+      hideRequiredMark
+      submitter={{
+        render: (props, dom) => {
+          return (
+            <FooterToolbar>
+              {getErrorInfo(error)}
+              {dom}
+            </FooterToolbar>
+          );
+        },
+      }}
+      initialValues={{
+        members: tableData,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <PageContainer content="فرم‌های پیشرفته معمولاً در سناریوهایی استفاده می‌شوند که مقادیر زیادی داده در یک زمان وارد و ارسال می‌شوند.">
+        <Card title="مدیریت انبار" className={styles.card} bordered={false}>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <ProFormText
+                label={fieldLabels.name}
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا نام انبار را وارد کنید',
+                  },
+                ]}
+                placeholder="لطفا نام انبار را وارد کنید"
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 6,
+                offset: 2,
+              }}
+              lg={{
+                span: 8,
+              }}
+              md={{
+                span: 12,
+              }}
+              sm={24}
+            >
+              <ProFormText
+                label={fieldLabels.url}
+                name="url"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا انتخاب کنید',
+                  },
+                ]}
+                fieldProps={{
+                  style: {
+                    width: '100%',
+                  },
+                  addonBefore: 'http://',
+                  addonAfter: '.com',
+                }}
+                placeholder="لطفا وارد کنید"
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 8,
+                offset: 2,
+              }}
+              lg={{
+                span: 10,
+              }}
+              md={{
+                span: 24,
+              }}
+              sm={24}
+            >
+              <ProFormSelect
+                label={fieldLabels.owner}
+                name="owner"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفاً یک مدیر انتخاب کنید',
+                  },
+                ]}
+                options={[
+                  {
+                    label: 'فو شیائوکسیائو',
+                    value: 'xiao',
+                  },
+                  {
+                    label: 'ژو مائومائو',
+                    value: 'mao',
+                  },
+                ]}
+                placeholder="لطفاً یک مدیر انتخاب کنید"
+              />
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <ProFormSelect
+                label={fieldLabels.approver}
+                name="approver"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفاً یک تأیید کننده انتخاب کنید',
+                  },
+                ]}
+                options={[
+                  {
+                    label: 'فو شیائوکسیائو',
+                    value: 'xiao',
+                  },
+                  {
+                    label: 'ژو مائومائو',
+                    value: 'mao',
+                  },
+                ]}
+                placeholder="لطفاً یک تأیید کننده انتخاب کنید"
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 6,
+                offset: 2,
+              }}
+              lg={{
+                span: 8,
+              }}
+              md={{
+                span: 12,
+              }}
+              sm={24}
+            >
+              <ProFormDateRangePicker
+                label={fieldLabels.dateRange}
+                name="dateRange"
+                fieldProps={{
+                  style: {
+                    width: '100%',
+                  },
+                }}
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا تاریخ انتخاب کنید',
+                  },
+                ]}
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 8,
+                offset: 2,
+              }}
+              lg={{
+                span: 10,
+              }}
+              md={{
+                span: 24,
+              }}
+              sm={24}
+            >
+              <ProFormSelect
+                label={fieldLabels.type}
+                name="type"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا نوع انبار را انتخاب کنید',
+                  },
+                ]}
+                options={[
+                  {
+                    label: 'خصوصی',
+                    value: 'private',
+                  },
+                  {
+                    label: 'عمومی',
+                    value: 'public',
+                  },
+                ]}
+                placeholder="لطفا نوع انبار را انتخاب کنید"
+              />
+            </Col>
+          </Row>
+        </Card>
+        <Card title="مدیریت کارها" className={styles.card} bordered={false}>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <ProFormText
+                label={fieldLabels.name2}
+                name="name2"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا وارد کنید',
+                  },
+                ]}
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 6,
+                offset: 2,
+              }}
+              lg={{
+                span: 8,
+              }}
+              md={{
+                span: 12,
+              }}
+              sm={24}
+            >
+              <ProFormText
+                label={fieldLabels.url2}
+                name="url2"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا انتخاب کنید',
+                  },
+                ]}
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 8,
+                offset: 2,
+              }}
+              lg={{
+                span: 10,
+              }}
+              md={{
+                span: 24,
+              }}
+              sm={24}
+            >
+              <ProFormSelect
+                label={fieldLabels.owner2}
+                name="owner2"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفاً یک مدیر انتخاب کنید',
+                  },
+                ]}
+                options={[
+                  {
+                    label: 'xiao',
+                    value: 'xiao',
+                  },
+                  {
+                    label: 'mao',
+                    value: 'mao',
+                  },
+                ]}
+              />
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col lg={6} md={12} sm={24}>
+              <ProFormSelect
+                label={fieldLabels.approver2}
+                name="approver2"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفاً یک تأیید کننده انتخاب کنید',
+                  },
+                ]}
+                options={[
+                  {
+                    label: 'xiao',
+                    value: 'xiao',
+                  },
+                  {
+                    label: 'mao',
+                    value: 'mao',
+                  },
+                ]}
+                placeholder="لطفاً یک تأیید کننده انتخاب کنید"
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 6,
+                offset: 2,
+              }}
+              lg={{
+                span: 8,
+              }}
+              md={{
+                span: 12,
+              }}
+              sm={24}
+            >
+              <ProFormTimePicker
+                label={fieldLabels.dateRange2}
+                name="dateRange2"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا وارد کنید',
+                  },
+                ]}
+                placeholder="زمان یادآوری"
+                fieldProps={{
+                  style: {
+                    width: '100%',
+                  },
+                }}
+              />
+            </Col>
+            <Col
+              xl={{
+                span: 8,
+                offset: 2,
+              }}
+              lg={{
+                span: 10,
+              }}
+              md={{
+                span: 24,
+              }}
+              sm={24}
+            >
+              <ProFormSelect
+                label={fieldLabels.type2}
+                name="type2"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا نوع انبار را انتخاب کنید',
+                  },
+                ]}
+                options={[
+                  {
+                    label: 'خصوصی',
+                    value: 'private',
+                  },
+                  {
+                    label: 'عمومی',
+                    value: 'public',
+                  },
+                ]}
+                placeholder="لطفا نوع انبار را انتخاب کنید"
+              />
+            </Col>
+          </Row>
+        </Card>
+        <Card title="مدیریت اعضا" bordered={false}>
+          <ProForm.Item name="members">
+            <EditableProTable<TableFormDateType>
+              recordCreatorProps={{
+                record: () => {
+                  return {
+                    key: `0${Date.now()}`,
+                  };
+                },
+              }}
+              columns={columns}
+              rowKey="key"
+            />
+          </ProForm.Item>
+        </Card>
+      </PageContainer>
+    </ProForm>
+  );
+};
+export default AdvancedForm;
---
+++ src/pages/form/step-form/index.tsx
@@ -0,0 +1,255 @@
+import {
+  PageContainer,
+  ProFormDigit,
+  ProFormSelect,
+  ProFormText,
+  StepsForm,
+} from '@ant-design/pro-components';
+import type { FormInstance } from 'antd';
+import { Alert, Button, Card, Descriptions, Divider, Result, Statistic } from 'antd';
+import React, { useRef, useState } from 'react';
+import type { StepDataType } from './data.d';
+import useStyles from './style.style';
+const StepDescriptions: React.FC<{
+  stepData: StepDataType;
+  bordered?: boolean;
+}> = ({ stepData, bordered }) => {
+  const { payAccount, receiverAccount, receiverName, amount } = stepData;
+  return (
+    <Descriptions column={1} bordered={bordered}>
+      <Descriptions.Item label="حساب پرداخت"> {payAccount}</Descriptions.Item>
+      <Descriptions.Item label="حساب های دریافتنی"> {receiverAccount}</Descriptions.Item>
+      <Descriptions.Item label="نام گیرنده پرداخت"> {receiverName}</Descriptions.Item>
+      <Descriptions.Item label="مقدار انتقال">
+        <Statistic
+          value={amount}
+          suffix={
+            <span
+            // style={{
+            //   fontSize: 14,
+            // }}
+            >
+              元
+            </span>
+          }
+          precision={2}
+        />
+      </Descriptions.Item>
+    </Descriptions>
+  );
+};
+
+const StepResult: React.FC<{
+  onFinish: () => Promise<void>;
+  children?: React.ReactNode;
+}> = (props) => {
+  const { styles } = useStyles();
+  return (
+    <Result
+      status="success"
+      title="عملیات موفقیت آمیز"
+      subTitle="انتظار می رود تا دو ساعت دیگر برسد"
+      extra={
+        <>
+          <Button type="primary" onClick={props.onFinish}>
+            مبلغ دیگری را انتقال دهید
+          </Button>
+          <Button>مشاهده صورتحساب</Button>
+        </>
+      }
+      className={styles.result}
+    >
+      {props.children}
+    </Result>
+  );
+};
+const StepForm: React.FC<Record<string, any>> = () => {
+  const { styles } = useStyles();
+  const [stepData, setStepData] = useState<StepDataType>({
+    payAccount: 'ant-design@alipay.com',
+    receiverAccount: 'test@example.com',
+    receiverName: 'Alex',
+    amount: '500',
+    receiverMode: 'alipay',
+  });
+  const [current, setCurrent] = useState(0);
+  const formRef = useRef<FormInstance>();
+
+  return (
+    <PageContainer content="یک کار طولانی یا ناآشنا را به چند مرحله تقسیم کنید و کاربران را از طریق آن راهنمایی کنید.">
+      <Card bordered={false}>
+        <StepsForm
+          containerStyle={{
+            width: '100%',
+          }}
+          // containerStyle={styles.containerStyle}
+          current={current}
+          onCurrentChange={setCurrent}
+          submitter={{
+            render: (props, dom) => {
+              if (props.step === 2) {
+                return null;
+              }
+              return dom;
+            },
+          }}
+        >
+          <StepsForm.StepForm<StepDataType>
+            formRef={formRef}
+            title="اطلاعات انتقال را پر کنید"
+            initialValues={stepData}
+            onFinish={async (values) => {
+              setStepData(values);
+              return true;
+            }}
+          >
+            <ProFormSelect
+              label="حساب پرداخت"
+              // width="md"
+              name="payAccount"
+              rules={[
+                {
+                  required: true,
+                  message: 'لطفا حساب پرداخت را انتخاب کنید',
+                },
+              ]}
+              valueEnum={{
+                'ant-design@alipay.com': 'ant-design@alipay.com',
+              }}
+            />
+
+            {/* <ProForm.Group title="حساب های دریافتنی" >
+              <ProFormSelect
+                name="receiverMode"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا حساب پرداخت را انتخاب کنید',
+                  },
+                ]}
+                valueEnum={{
+                  alipay: 'Alipay',
+                  bank: 'حساب بانکی',
+                }}
+
+              />
+              <ProFormText
+                name="receiverAccount"
+                rules={[
+                  {
+                    required: true,
+                    message: 'لطفا حساب دریافت کننده را وارد کنید',
+                  },
+                  {
+                    type: 'email',
+                    message: 'نام حساب کاربری باید در قالب ایمیل باشد',
+                  },
+                ]}
+                placeholder="test@example.com"
+              />
+            </ProForm.Group> */}
+            <ProFormText
+              label="نام گیرنده پرداخت"
+              // width="md"
+              name="receiverName"
+              rules={[
+                {
+                  required: true,
+                  message: 'لطفا نام گیرنده پرداخت را وارد کنید',
+                },
+              ]}
+              placeholder="لطفا نام گیرنده پرداخت را وارد کنید"
+            />
+            <ProFormDigit
+              label="مقدار انتقال"
+              name="amount"
+              // width="md"
+              rules={[
+                {
+                  required: true,
+                  message: 'لطفا مبلغ انتقال را وارد کنید',
+                },
+                {
+                  pattern: /^(\d+)((?:\.\d+)?)$/,
+                  message: 'لطفا یک عدد مبلغ قانونی وارد کنید',
+                },
+              ]}
+              placeholder="لطفا مبلغ را وارد کنید"
+              fieldProps={{
+                prefix: '￥',
+              }}
+            />
+            <Divider
+              style={{
+                margin: '24px 0',
+              }}
+            />
+          </StepsForm.StepForm>
+
+          <StepsForm.StepForm title="اطلاعات انتقال را تایید کنید">
+            <div className={styles.result}>
+              <Alert
+                closable
+                showIcon
+                message="پس از تایید انتقال، وجوه مستقیماً به حساب طرف مقابل واریز می شود و قابل برگشت نیست."
+                // style={{
+                //   marginBottom: 24,
+                // }}
+              />
+              <StepDescriptions stepData={stepData} bordered />
+
+              <ProFormText.Password
+                label="رمز پرداخت"
+                // width="md"
+                name="password"
+                required={false}
+                rules={[
+                  {
+                    required: true,
+                    message: 'برای پرداخت به رمز عبور پرداخت نیاز است',
+                  },
+                ]}
+              />
+              <Divider
+                style={{
+                  margin: '24px 0',
+                }}
+              />
+            </div>
+          </StepsForm.StepForm>
+          <StepsForm.StepForm title="پایان">
+            <StepResult
+              onFinish={async () => {
+                setCurrent(0);
+                formRef.current?.resetFields();
+              }}
+            >
+              <StepDescriptions stepData={stepData} />
+            </StepResult>
+          </StepsForm.StepForm>
+        </StepsForm>
+        {/* <Divider
+          style={{
+            margin: '40px 0 24px',
+          }}
+        /> */}
+        <div>
+          <h3>نشان دادن</h3>
+          <h4>انتقال به حساب Alipay</h4>
+          <p>
+            در صورت نیاز، می توانید سوالات متداول در مورد محصول را در اینجا قرار دهید. در صورت نیاز،
+            می توانید سوالات متداول در مورد محصول را در اینجا قرار دهید. در صورت نیاز، می توانید
+            سوالات متداول در مورد محصول را در اینجا قرار دهید.
+          </p>
+          <h4>انتقال به کارت بانکی</h4>
+          <p>
+            در صورت نیاز، می توانید سوالات متداول در مورد محصول را در اینجا قرار دهید. در صورت نیاز،
+            می توانید سوالات متداول در مورد محصول را در اینجا قرار دهید. در صورت نیاز، می توانید
+            سوالات متداول در مورد محصول را در اینجا قرار دهید.
+          </p>
+        </div>
+      </Card>
+    </PageContainer>
+  );
+};
+export default StepForm;
